fixed bugs in backpropagation

Generic-Neural-Network/mlp.py
@@ -84,24 +84,46 @@ class Mlp:
             for i in range(len(dataset)):
                 Xi =  dataset[i] [0:self.n_neurons_input]
                 Yi =  dataset[i][self.n_neurons_input:len(dataset[0])]
+
+                #print("entrada: ",Xi)
+                #print("saida: ",Yi)
+
                 self.feed_forward(Xi)
 
                 error = np.array(Yi) - np.array(self.output_f_nets)
+
+                #print("error: ",error)
                 squaredError += np.sum(np.power(error, 2))
 
                 output_delta = error * self.df_dnet(self.output_f_nets)
+                #print("output delta\n", output_delta)
 
+                #for each neuron in the output layer
+                """
                 for i in range(self.output_layer_weights_and_theta.shape[0]):
-                    hidden_delta = np.multiply(self.df_dnet(self.hidden_f_nets),np.dot(np.matrix(output_delta), np.matrix(self.output_layer_weights_and_theta[i, 0:self.n_neurons_hiddens])))
+                    print(np.matrix(self.output_layer_weights_and_theta))
+                    print(np.matrix(output_delta))
+                    print(self.hidden_f_nets)
+                    #delta versus
+                    #temp = np.dot(np.matrix(output_delta), np.matrix(self.output_layer_weights_and_theta[i, :]))
+                    #print(temp)
+                    #hidden_delta = np.multiply(self.df_dnet(self.hidden_f_nets),np.dot(np.matrix(output_delta), np.matrix(self.output_layer_weights_and_theta[i, 0:self.n_neurons_hiddens])))
+                """
 
-                self.output_layer_weights_and_theta = self.output_layer_weights_and_theta + eta*(np.dot(np.matrix(output_delta), np.matrix(np.append(self.hidden_f_nets, 1))))
+                hidden_delta = np.multiply(self.df_dnet(self.hidden_f_nets),np.dot(np.matrix(output_delta), np.matrix(self.output_layer_weights_and_theta[:, 0:self.n_neurons_hiddens])))
+
+                #print(hidden_delta)
+                #print("output layer weights",self.output_layer_weights_and_theta)
+                self.output_layer_weights_and_theta = self.output_layer_weights_and_theta + eta*(np.dot(np.transpose(np.matrix(output_delta)), np.matrix(np.append(self.hidden_f_nets, 1))))
                 aux = eta*np.dot(np.matrix(hidden_delta).T, np.matrix(Xi))
 
                 self.hidden_layer_weights_and_theta = self.hidden_layer_weights_and_theta + aux
             
             squaredError = squaredError/len(dataset)
 
-            print("Erro", squaredError)
+            #print("Erro", squaredError)
+
+        print("Training done")
 
     # Mostra a rede neural
     def show(self):