bedfcc02bc9492beb669344b099abe94721d2b09
elisasaltori <elisa.trujillo@usp.br>
2018-10-16 09:21:21 -0300

    loading and saving model and new architecture

--- CIFAR10/cifar10_project.py
+++ CIFAR10/cifar10_project.py
@@ -12,6 +12,7 @@ Exercício 4
 import tensorflow as tf
 from tensorflow import keras
 from keras import backend as K
+from keras import optimizers
 
 # Helper libraries
 import numpy as np
@@ -25,6 +26,10 @@ import glob
 
 
 test_directory = "test_images"
+label_names = ["airplane", "automobile", "bird", "cat",
+"deer", "dog", "frog", "horse", "ship", "truck"]
+model_name = 'keras_cifar10_trained_model.h5'
+
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -36,13 +41,20 @@ n_channels = 3
 def main():
 
   #train model
-  model = train()
+  #model = train()
+  model = load_model()
 
   #test on saved images
   test_on_saved_images(model)
 
 
 def train():
+
+  save_dir = os.path.join(os.getcwd(), 'saved_models')
+  
+
+  batch_size = 32
+  epochs = 20
 
   #loading data
   cifar10 = keras.datasets.cifar10 
@@ -66,28 +78,46 @@ def train():
 
   #building model
   model = keras.Sequential([
-    keras.layers.Conv2D(8, kernel_size=(8, 8), activation='relu', input_shape=input_shape),
-    keras.layers.Conv2D(16, (4, 4), activation='relu'),
+    keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
+    keras.layers.Conv2D(32, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Dropout(0.25),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.Dropout(0.25),
     keras.layers.Flatten(),
-    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dropout(0.5),
     keras.layers.Dense(10, activation=tf.nn.softmax)
   ])
 
-  model.compile(optimizer=tf.train.AdamOptimizer(), 
+  model.compile(optimizer=keras.optimizers.Adam(lr=0.0001, decay=1e-6), 
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
   #train model
-  model.fit(train_images, train_labels, epochs=1)
+  #model.fit(train_images, train_labels, epochs=10)
+  model.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, shuffle=True)
 
   #test accuracy
   test_loss, test_acc = model.evaluate(test_images, test_labels)
 
   print('Test accuracy:', test_acc)
 
+  if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+  model_path = os.path.join(save_dir, model_name)
+  model.save(model_path)
+  print('Saved trained model at %s ' % model_path)
+
   return model
 
+
+def load_model():
+
+  filepath = os.path.join(os.getcwd(), 'saved_models', model_name)
+  model = keras.models.load_model(filepath)
+  return model
 
 def test_on_saved_images(model):
   """
@@ -126,7 +156,7 @@ def test_on_saved_images(model):
 
   for i in range(0, predictions.shape[0]):
     print(str(labels[i])+":", predictions[i])
-    print("Result:", np.argmax(predictions[i]))
+    print("Result:", np.argmax(predictions[i]), "("+label_names[np.argmax(predictions[i])]+")")
 
 
 if __name__ == "__main__":
