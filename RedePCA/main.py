247e79d9e0eafdeda3d87f0c026b3c999d428be7
elisasaltori <elisa.trujillo@usp.br>
2018-11-11 16:56:19 -0200

    added mlp + images comparison

--- RedePCA/main.py
+++ RedePCA/main.py
@@ -2,50 +2,143 @@ from pca_network import PCA_Network
 import sklearn.datasets as dt
 import numpy as np
 import tensorflow as tf
-#from tensorflow import keras
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
-N_COMPONENTS = 3
+N_COMPONENTS = 8
 
 def main():
 
-	pca_n = PCA_Network(13, N_COMPONENTS)
+    #get wine dataset
+    data, target = dt.load_wine(return_X_y=True)
+    data = np.matrix(data)
 
-	data, target = dt.load_wine(return_X_y=True)
-	data = np.matrix(data)
+    #normalize it
+    norm = (data - np.mean(data, axis=0))/np.std(data, axis=0)
 
-	norm = (data - np.mean(data, axis=0))/np.std(data, axis=0)
+    #initialize and train PCA network
+    pca_n = PCA_Network(13, N_COMPONENTS)
+    pca_n.train(norm)
+    
+    #PCA from network
+    print("PCA adaptativa:")
+    print(np.transpose(pca_n.input_weights))
+    print()
 
-	
-	#pca_n.train(norm)
+    #Traditional PCA
+    _, _, eig_vecs = pca(norm)
+    print("PCA")
+    print(eig_vecs[:,:N_COMPONENTS])
+    print()
 
-	print("PCA adaptativa:")
-	print(np.transpose(pca_n.input_weights))
-	print()
 
-	_, _, eig_vecs = pca(norm)
-	print("PCA")
-	print(eig_vecs[:,:N_COMPONENTS])
-	print()
+    fig, ax = plt.subplots()
+    proj_data = np.dot(norm,eig_vecs)
+    color = ['#1b9e77', '#d95f02', '#7570b3']
+    for idx, val in enumerate(np.unique(target)):
+        index = np.argwhere(target==val)
+        ax.scatter([proj_data[index,0]], [proj_data[index,1]], c = color[idx], label=val)
 
-	#projetar dados via PCA adaptativa
-	output = feed_net(norm, pca_n)
 
-	#treinar e testar via MPL
-	#	dados originais (normalizados)
-	#	dados projetados
+    ax.legend()
+    plt.title("PCA - Projeção sobre as duas principais componentes")
+    plt.tight_layout()
+    plt.show()
+
+
+    #project data with adaptive PCA
+    output = feed_net(norm, pca_n)
+
+    fig, ax = plt.subplots()
+    color = ['#1b9e77', '#d95f02', '#7570b3']
+    for idx, val in enumerate(np.unique(target)):
+        index = np.argwhere(target==val)
+        ax.scatter([output[index,0]], [output[index,1]], c = color[idx], label=val)
+
+
+    ax.legend()
+    plt.title("PCA Adaptativa - Projeção sobre as duas principais componentes")
+    plt.tight_layout()
+    plt.show()
+
+
+    #formating output for use in network
+    target_cat =  keras.utils.to_categorical(target, 3)
+
+
+    #Train and test with MLPs
+    #	the original data
+    train_test_original_dataset(norm, target_cat)
+
+    #	projected data
+    train_test_pca(output, target_cat)
+
 
 
 def feed_net(data, pca_n):
+    """
+    Feed all the rows of data to the pca network and get
+    the projected data output
 
-	output = np.zeros((len(data), N_COMPONENTS))
-	#print(output)
+    """
+    output = np.zeros((len(data), N_COMPONENTS))
 
-	for i in range(0, len(data)):
-		output[i] = np.transpose(pca_n.feed_forward(data[i]))
+    for i in range(0, len(data)):
+        output[i] = np.transpose(pca_n.feed_forward(data[i]))
 
-	return output
+    return output
 
 
+def train_test_original_dataset(norm, target_cat):
+    """
+    Train a MLP network on the original data and get accuracy
+    """
+    x_train, x_test, y_train, y_test = train_test_split(norm, target_cat, test_size=0.15)
+
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(10, input_dim=13, activation='relu'))
+    model.add(keras.layers.Dense(8, activation='relu'))
+    model.add(keras.layers.Dense(6, activation='relu'))
+    model.add(keras.layers.Dense(6, activation='relu'))
+    model.add(keras.layers.Dense(4, activation='relu'))
+    model.add(keras.layers.Dense(2, activation='relu'))
+    model.add(keras.layers.Dense(3, activation='softmax'))
+
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+
+    model.fit(x_train, y_train, batch_size=15, epochs=1000, verbose=0)
+
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+
+    print('Original dataset - test accuracy:', test_acc)
+
+
+def train_test_pca(pca_data, target_cat):
+    """
+    Train a MLP network on pca-projected data and get accuracy
+    """
+    x_train, x_test, y_train, y_test = train_test_split(pca_data, target_cat, test_size=0.15)
+
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(10, input_dim=N_COMPONENTS, activation='relu'))
+    model.add(keras.layers.Dense(8, activation='relu'))
+    model.add(keras.layers.Dense(6, activation='relu'))
+    model.add(keras.layers.Dense(6, activation='relu'))
+    model.add(keras.layers.Dense(4, activation='relu'))
+    model.add(keras.layers.Dense(2, activation='relu'))
+    model.add(keras.layers.Dense(3, activation='softmax'))
+
+    
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+
+    model.fit(x_train, y_train, batch_size=15, epochs=1000, verbose=0)
+
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+
+    print('PCA dataset - test accuracy:', test_acc)
 
 def pca(data):
 	"""
