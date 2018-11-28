e9f45e01025209ef88eee100a9fdba92690523d0
Vitor Giovani Dellinocente <vitor.dellinocente@usp.br>
2018-11-28 17:46:10 -0200

    distance problem

--- Kohonen/kohonen.py
+++ Kohonen/kohonen.py
@@ -1,9 +1,13 @@
 import numpy as np
+import math
 import sklearn.datasets as dt
 from matplotlib import pyplot as plt
 
-ALPHA = 0.5
+#ALPHA = 0.5
 EPSILON = 1.5
+
+def update_learning_rate(alpha, iter, max_iter):
+        return alpha * math.exp((-iter)/max_iter)
 
 def bmu(x, y):
         dist_matrix = np.zeros((x.shape[0], x.shape[1]))
@@ -13,15 +17,15 @@ def bmu(x, y):
 
         index = np.argmin(dist_matrix)
         index =  int(index/x.shape[0]), int(index % x.shape[1])
-        
+        print(np.amin(dist_matrix))
         return index
 
-def update_neighbours(index, weights, neighbourhood, input_data):
+def update_neighbours(index, weights, neighbourhood, input_data, alpha):
         neighbourhood = neighbourhood + 1
         dir = [[1,0], [1,1], [0,1], [-1, 1], [-1,0], [-1,-1], [0,-1], [1,-1]]
 
         #update weights in index
-        weights[index] = weights[index] + ALPHA*(input_data - weights[index])
+        weights[index] = weights[index] + alpha*(input_data - weights[index])
 
         #update weights in neghbourhood
         for i in range(neighbourhood):
@@ -31,21 +35,21 @@ def update_neighbours(index, weights, neighbourhood, input_data):
                         if(x < weights.shape[0] and x >= 0):
                                 if(y < weights.shape[1] and y >= 0):
                                         #print((i+1)*dir[j][0] + index[0], (i+1)*dir[j][1] + index[1])
-                                        weights[x][y] = weights[x][y] + (1/(i+1) + EPSILON)*ALPHA*(input_data - weights[x, y])
+                                        weights[x][y] = weights[x][y] + (1/(i+1) + EPSILON)*alpha*(input_data - weights[x, y])
 
         
         
 def init_weight(nattributes, nrow=10, ncol=10):
         return np.random.rand(nrow, ncol, nattributes)
 
-def train(data, weights, max_iter=100):
+def train(data, weights, max_iter=100, alpha=0.5):
         iter = 0
 
         while(iter < max_iter):
                 for row in data:
                         index = bmu(weights, row)
-                        update_neighbours(index, weights, 1, row)
-
+                        update_neighbours(index, weights, 5, row, alpha)
+                alpha = update_learning_rate(alpha, iter, max_iter)
                 iter = iter + 1
                 print("iteration " + str(iter) + "/" + str(max_iter))
 
@@ -58,9 +62,12 @@ def main():
     data = np.asmatrix(data)
     weights = init_weight(data.shape[1], nrow=3, ncol=3)
     print(weights)
-    train(data, weights)
+    train(data, weights, max_iter=10)
     weights = (weights - np.amin(weights))/(np.amax(weights) - np.amin(weights))
     print(weights)
+    #print()
+    #print(np.amax(weights))
+    #print(np.amin(weights))
 
     
 
